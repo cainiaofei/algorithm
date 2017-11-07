84092f6826f5bf6ac56ba68febe8c7234ac8eafd
zzf <zhznfei@yeah.net>
2017-11-07 15:27:02 +0800

    greedy algorithm

--- src/main/java/cn/nju/algorithm/hiho/Main.java
+++ src/main/java/cn/nju/algorithm/hiho/Main.java
@@ -1,5 +1,54 @@
 package cn.nju.algorithm.hiho;
 
-public class Main {
+import java.util.Arrays;
+import java.util.Scanner;
 
+/**
+ * @author zzf 
+ * @date 2017.10.07
+ * @description Robots Crossing River, the bottleneck of this problem is from B to C.
+ */
+public class Main {
+	
+	public void process() {
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		int z = scan.nextInt();
+		scan.close();
+		int timeCost = calculateMinTime(x,y,z);
+		System.out.println(timeCost);
+	}
+	
+	private int calculateMinTime(int x, int y, int z) {
+		int[] nums = {x,y,z};
+		Arrays.sort(nums);
+		int times = 0;
+		if(nums[2]<=(nums[1]+nums[0])) {
+			times = (int)Math.ceil((x+y+z)*1.0/20);
+		}
+		else {//greedy
+			int one = nums[2];
+			int other = nums[1]+nums[0];
+			while(other>=10) {
+				one -= 10;
+				other -= 10;
+				times++;
+			}
+			if(other<8) {
+				one -= (15-other);
+			}
+			else {
+				one -= other;
+			}
+			times++;
+			times += (int)Math.ceil(one*1.0/15);
+		}
+		return times * 6;
+	}
+
+	public static void main(String[] args) {
+		Main m = new Main();
+		m.process();
+	}
 }
